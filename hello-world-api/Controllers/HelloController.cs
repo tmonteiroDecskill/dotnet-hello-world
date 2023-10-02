f485315d07a80ff91bb2bc98f7178f9daf18fdd7
tmonteiroDecskill <143775533+tmonteiroDecskill@users.noreply.github.com>
2023-10-02 19:26:57 +0100

    bugfix

--- hello-world-api/Controllers/HelloController.cs
+++ hello-world-api/Controllers/HelloController.cs
@@ -9,11 +9,11 @@ namespace hello_world_api.Controllers
     [Route("api/[controller]")]
     public class HelloController : Controller
     {
-        // GET api/helllo
+        // GET api/helllo  http://localhost:5000/api/hello
         [HttpGet]
         public string Get()
         {
-            return "Hello world!!!!!!";
+            return "Hello world!!!!!";
         }
 
     }
